ce3c31c1943aaf83c1e1122612705d6281e1f396
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2023-04-24 19:47:39 +0800

    add tests

--- tools/demo/sqlite-quick/index.ts
+++ tools/demo/sqlite-quick/index.ts
@@ -3,6 +3,8 @@ import { DemoSharedBase } from '../utils';
 import {} from '@nativescript/sqlite-quick';
 
 export class DemoSharedSqliteQuick extends DemoSharedBase {
+  db;
+
   testIt() {
     console.log('test sqlite-quick!');
 
@@ -12,6 +14,30 @@ export class DemoSharedSqliteQuick extends DemoSharedBase {
       console.log('creating module');
     }
     global.QuickSQLiteImpl.open('db2.sqlite');
-    console.log('maybe it did a thing');
+    console.log('maybe it did a thing?', this.db);
+  }
+
+  addTable() {
+    global.QuickSQLiteImpl.execute('DROP TABLE IF EXISTS User;');
+    global.QuickSQLiteImpl.execute('CREATE TABLE User ( id INT PRIMARY KEY, name TEXT NOT NULL, age INT, networth REAL) STRICT;');
+  }
+
+  addRow() {
+    const id = 1;
+    const name = 'Test';
+    const age = 22;
+    const networth = 123.0001;
+    const res = this.db.execute('INSERT INTO "User" (id, name, age, networth) VALUES(?, ?, ?, ?)', [id, name, age, networth]);
+    console.log(res);
+  }
+
+  getRows() {
+    const id = Math.round(Math.random() * 100);
+    const name = 'chance.name()';
+    const age = Math.round(Math.random() * 100);
+    const networth = Math.random() * 10000;
+    const res = this.db.execute('INSERT INTO "User" (id, name, age, networth) VALUES(?, ?, ?, ?)', [id, name, age, networth]);
+
+    console.log(res);
   }
 }
